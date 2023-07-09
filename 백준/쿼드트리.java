6825e26f7ad675a2e423c5b0a433ceeb66617882
정재윤 <tomato8269@naver.com>
2023-07-09 22:01:50 +0900

    백준 1992번: 쿼드트리

---
+++ 백준/쿼드트리.java
@@ -0,0 +1,53 @@
+/*
+ * 백준 1992번: 쿼드트리
+ * 알고리즘 분류: #재귀 #분할정복
+ */
+import java.io.*;
+
+public class Main { 
+	static StringBuilder sb = new StringBuilder();
+	static int[][] map;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		
+		for(int i=0;i<N;i++) {
+			String s = br.readLine();
+			for(int j=0;j<N;j++) {
+				map[i][j] = s.charAt(j)-'0';
+			}
+		}
+		quadTree(N,0,0);
+		System.out.println(sb);
+	}
+	
+	public static void quadTree(int N, int row, int column) {
+		boolean state = true;
+		int color = map[row][column];
+		
+		for(int i=row;i<row+N;i++) {	//0 혹은 1로 압축할 수 있는지 확인
+			for(int j=column;j<column+N;j++) {
+				if(map[i][j]!=color) {
+					state=false;
+				}
+			}
+		}
+		
+		if(state) {		//압축이 가능하다면 압축한 결과를 추가함
+			sb.append(map[row][column]);
+		}
+		else {		//압축이 불가능하다면 각 영역별로 4번 재귀호출
+			sb.append('(');
+			quadTree(N/2, row, column);
+			quadTree(N/2, row, column+N/2);
+			quadTree(N/2, row+N/2, column);
+			quadTree(N/2, row+N/2, column+N/2);
+			sb.append(')');
+		}
+		
+		return;
+	}
+}

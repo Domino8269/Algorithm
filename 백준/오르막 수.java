634d31ea8f81f445202e753b8ac5634f22348320
saebyeok2 <tomato8269@naver.com>
2024-05-09 17:21:32 +0900

    백준 11057번 오르막 수

---
+++ 백준/오르막 수.java
@@ -0,0 +1,32 @@
+/**
+ * 백준 11057번 - 오르막 수
+ * 알고리즘 분류 - DP
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N][10];
+
+        for (int i = 0; i < 10; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = j; k < 10; k++) {
+                    dp[i][j] += dp[i - 1][k];
+                    dp[i][j] %= 10007;
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(dp[N - 1]).sum() % 10007);
+    }
+}
